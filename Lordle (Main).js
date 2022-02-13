a25da49f7c2e79e7dc4bc95a1c2939d5d6b2800c
DylanPanRL <99445654+DylanPanRL@users.noreply.github.com>
2022-02-13 00:33:00 -0500

    Update Lordle (Main).js

--- Lordle (Main).js
+++ Lordle (Main).js
@@ -1145,7 +1145,7 @@ this.wordle.bundle = function(e) {
           , s = e % 100;
         return e + (a[(s - 20) % 10] || a[s] || a[0])
     }
-    var Ha = new Date(2022,1,15,0,0,0,0);
+    var Ha = new Date(2022,2,13,0,0,0,0);
     function Na(e, a) {
         var s = new Date(e)
           , t = new Date(a).setHours(0, 0, 0, 0) - s.setHours(0, 0, 0, 0);
